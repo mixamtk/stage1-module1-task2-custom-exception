f3475328de1c0e643b1ad251b3b8bce21a07890d
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-09-16 17:09:07 +0300

    Update StudentIdException.java

--- src/main/java/com/epam/mjc/StudentIdException.java
+++ src/main/java/com/epam/mjc/StudentIdException.java
@@ -1,8 +1,7 @@
 package com.epam.mjc;
 
 public class StudentIdException extends  IllegalArgumentException {
-    private final static String EX_HEAD = "Could not find student with ID {";
-    private final static String EX_TAIL = "}";
+    private final static String EX_HEAD = "Could not find student with ID ";
     private long id;
 
     public StudentIdException(long id) {
@@ -15,6 +14,6 @@ public class StudentIdException extends  IllegalArgumentException {
 
     @Override
     public String toString() {
-        return EX_HEAD + id + EX_TAIL;
+        return EX_HEAD + id;
     }
 }
